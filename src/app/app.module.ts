8857bec0fd9eda20a6937d08001267f9ec270fa7
Edson Ajeje <edson.ajeje@terra.com.br>
2020-12-28 21:40:30 -0300

    implementação app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,42 @@
+import { ReplacePipe } from './pipe/replace.pipe';
+import { StarComponent } from './star/star.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CourseListComponent } from './courses/course-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    CourseListComponent,
+    NavBarComponent,
+    PageNotFoundComponent,
+    ReplacePipe,
+    StarComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'courses',
+        pathMatch: 'full'
+      },
+      {
+        path: 'courses',
+        component: CourseListComponent
+      },
+      {
+        path: '**',
+        component: PageNotFoundComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
